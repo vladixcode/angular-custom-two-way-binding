fd8fc7d7d49b175b73916cbd37a4034eca0bbe51
Vladimir Ilic <vladimir.ilic101@gmail.com>
2025-02-28 11:55:21 +0100

    Newer & simpler way of setting custom Two-Way bindable property for Angular >= 17.2

--- src/app/rect/rect.component.ts
+++ src/app/rect/rect.component.ts
@@ -1,5 +1,12 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core'
-
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// eslint-disable-next-line prettier/prettier
+import {
+  Component,
+  EventEmitter,
+  Input,
+  model,
+  Output
+} from '@angular/core'
 @Component({
   selector: 'app-rect',
   standalone: true,
@@ -8,12 +15,20 @@ import { Component, EventEmitter, Input, Output } from '@angular/core'
   styleUrl: './rect.component.css',
 })
 export class RectComponent {
-  // Custom two-way binding
-  @Input({ required: true }) size!: { width: string; height: string }
-  @Output() sizeChange = new EventEmitter<{ width: string; height: string }>()
+  // Custom two-way binding with Decorators for Angular < 17.2
+  // @Input({ required: true }) size!: { width: string; height: string }
+  // @Output() sizeChange = new EventEmitter<{ width: string; height: string }>()
+
+  // Custom two-way binding with model() function for Angular >= 17.2
+  size = model.required<{ width: string; height: string }>()
 
   onReset() {
-    this.sizeChange.emit({
+    // this.sizeChange.emit({
+    //   width: '300',
+    //   height: '300',
+    // })
+
+    this.size.set({
       width: '300',
       height: '300',
     })
